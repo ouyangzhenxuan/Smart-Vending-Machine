8e4329bdc1a309c2b6537c972d2ce3720536fef1
BigMX <31287694+BigMX@users.noreply.github.com>
2020-02-08 12:34:09 -0600

    Feb 8

--- FirebaseExample/functions/index.js
+++ FirebaseExample/functions/index.js
@@ -2,6 +2,19 @@ const functions = require('firebase-functions');
 const express = require('express');
 var firebase = require('firebase');
 const crypto = require('crypto')
+var nodemailer = require('nodemailer');
+
+
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    auth: {
+        user: 'smartvvvmachine@gmail.com',
+        pass: 'swvault123'
+    }
+});
 
 var config = {
     apiKey: "AIzaSyDEg1WylC8trdpBbfqKLRA4zB0jy0jPnic",
@@ -17,6 +30,9 @@ var config = {
   firebase.initializeApp(config);
 
 const app = express();
+
+const routes = require('./routes');
+app.use('/', routes);
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
@@ -40,6 +56,11 @@ function writeUserData(name, email, password) {
     }
   }
 
+app.use('/test', require('./routes'));
+app.use('/getprice', require('./routes'));
+
+// app.use('/', require('./routes/index'));
+
 app.post('/signup', (request, response) => {
     console.log("HTTP Get Request");
     // response.send('HTTP GET Request');
@@ -50,22 +71,22 @@ app.post('/signup', (request, response) => {
     var email=request.body.email;
 
     let hash = crypto.createHash('md5').update(email).digest("hex")
-    var ref = firebase.database().ref('users');
-    ref.on("value", function(snapshot) {
-        var password = snapshot.child(hash).child('password').val();
-    
-    if(password==null){
-        firebase.database().ref('users/' + hash).set({
-            username: name,
-            email: email,
-            password : password
-          });
-        response.send("complete");
-    }else{
-        response.send("user already exist");
-    }
+    var refer = firebase.database().ref('users');
+    var pass;
+    refer.on("value", function(snapshot) {
+        pass = snapshot.child(hash).child('password').val();
+        console.log(pass);
     });
-    // response.send(request.body.name);
+        if(pass==null){
+            firebase.database().ref('users/' + hash).set({
+                username: name,
+                email: email,
+                password : password
+            });
+            response.send("complete");
+        }else{
+            response.send("user already exist");
+        }
 });
 
 app.post('/login', (request, response)=>{
@@ -93,7 +114,68 @@ app.get('/timestamp', (request, response) => {
     response.send(`123`);
 });
 
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
 
+app.post('/sendEmail', (request, response) => {
+    var tempPassword = makeid(6);
+    var email=request.body.email;
+    var mailOptions = {
+        from: 'smartvvvmachine@gmail.com',
+        to: email,
+        subject: 'Sending Email using Node.js',
+        text: 'Your Passowrd Has Been Updated!\nTemporary Password:'+tempPassword+'\n\nSmart Vending Machine',
+        html: '<div>Your Passowrd Has Been Updated!</div><div>Temporary Pin:<b>'+tempPassword+'</b></div><hr><div>Smart Vending Machine</div>'
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+          response.send(error)
+        } else {
+          console.log('Email sent: ' + info.response);
+          response.send("okay");
+        }
+    });
+    let hash = crypto.createHash('md5').update(email).digest("hex")
+
+    firebase.database().ref('temporaryPin/' + hash).set({
+        email: email,
+        pin: tempPassword,
+    });
+});
+
+app.post('/changePassword', (request, response) => {
+    var new_password=request.body.password;
+    var email=request.body.email;
+    var input_pin = request.body.pin;
+    console.log(email)
+    let hash = crypto.createHash('md5').update(email).digest("hex")
+    console.log(hash)
+    var ref_pin = firebase.database().ref('temporaryPin');
+    var inside_pin;
+    ref_pin.on("value", function(snapshot) {
+        if(snapshot.child(hash).child('pin').val()==input_pin && input_pin!=null){
+            firebase.database().ref('users/' + hash).update({
+                password : new_password,
+            });
+            response.send("200");
+            console.log(new_password);
+        }else{
+            response.send('WRONG')
+        }
+        
+    },function (error){
+        response.send(error);
+    });
+    
+});
 
  // Create and Deploy Your First Cloud Functions
  // https://firebase.google.com/docs/functions/write-firebase-functions
---
+++ FirebaseExample/functions/routes/index.js
@@ -0,0 +1,87 @@
+const routes = require('express').Router();
+
+routes.get('/test', (req, res) => {
+    res.status(200).json({ message: 'Connected!' });
+  });
+  
+routes.get('/getprice', (request, response)=>{
+    const puppeteer = require('puppeteer');
+    var theItem;
+    theItem = puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox', '--window-size=1920,1080','--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'] }).then(async browser => {
+ 
+        const page = await browser.newPage();
+        await page.goto("https://www.amazon.com/Apple-iPhone-XR-Fully-Unlocked/dp/B07P6Y7954");
+        await page.waitForSelector('body');
+ 
+        var productInfo = await page.evaluate(() => {
+ 
+            /* Get product title */
+            let title = document.body.querySelector('#productTitle').innerText;
+ 
+            /* Get review count */
+            let reviewCount = document.body.querySelector('#acrCustomerReviewText').innerText;
+            let formattedReviewCount = reviewCount.replace(/[^0-9]/g,'').trim();
+ 
+            /* Get and format rating */
+            let ratingElement = document.body.querySelector('.a-icon.a-icon-star').getAttribute('class');
+            let integer = ratingElement.replace(/[^0-9]/g,'').trim();
+            let parsedRating = parseInt(integer) / 10;
+ 
+            /* Get availability */
+            let availability = document.body.querySelector('#availability').innerText; 
+            let formattedAvailability = availability.replace(/[^0-9]/g, '').trim();
+ 
+            /* Get list price */
+            let listPrice = document.body.querySelector('.priceBlockStrikePriceString').innerText;
+ 
+            /* Get price */
+            let price = document.body.querySelector('#priceblock_ourprice').innerText;
+ 
+            /* Get product description */
+            let description = document.body.querySelector('#renewedProgramDescriptionAtf').innerText;
+ 
+            /* Get product features */
+            let features = document.body.querySelectorAll('#feature-bullets ul li');
+            let formattedFeatures = [];
+ 
+            features.forEach((feature) => {
+                formattedFeatures.push(feature.innerText);
+            });
+ 
+            /* Get comparable items */
+            let comparableItems = document.body.querySelectorAll('#HLCXComparisonTable .comparison_table_image_row .a-link-normal');                
+            formattedComparableItems = [];
+ 
+            comparableItems.forEach((item) => {
+                formattedComparableItems.push("https://amazon.com" + item.getAttribute('href'));
+            });
+ 
+ 
+            var product = { 
+                "title": title,
+                "rating": parsedRating,
+                "reviewCount" : formattedReviewCount,
+                "listPrice": listPrice,
+                "price": price,
+                "availability": formattedAvailability,
+                "description": description,
+                "features": formattedFeatures,
+                "comparableItems": formattedComparableItems
+            };
+            return product;
+ 
+        });
+ 
+            console.log(productInfo);
+            var product = {
+                "title": productInfo['title'],
+                "price": productInfo['price'],
+            }
+            await browser.close();
+            result = product;
+            response.send(product);
+        }).catch(function(error) {
+            console.error(error);
+    });
+ });
+module.exports = routes;
